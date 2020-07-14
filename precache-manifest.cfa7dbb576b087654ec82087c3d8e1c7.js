813dbb8e39712ae9f24900f27c05ab39ce52bb7c
Oscar Alfredo  Cruz Rovira <cruzrovira@gmail.com>
2020-07-14 15:56:41 -0600

    Updates

---
+++ precache-manifest.cfa7dbb576b087654ec82087c3d8e1c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1844009481bd6c35280f79ef2e7a54bf",
+    "url": "/safe/index.html"
+  },
+  {
+    "revision": "8e3efb5159d898375f08",
+    "url": "/safe/static/css/main.a80513b8.chunk.css"
+  },
+  {
+    "revision": "f1955f938249f2470ea2",
+    "url": "/safe/static/js/2.1c1f9317.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/safe/static/js/2.1c1f9317.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3efb5159d898375f08",
+    "url": "/safe/static/js/main.67cf10bf.chunk.js"
+  },
+  {
+    "revision": "36eff0d4211414c46bc9",
+    "url": "/safe/static/js/runtime-main.3ec40cf1.js"
+  }
+]);
